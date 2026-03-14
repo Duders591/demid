4a4e64b14cecf0b3173d9ff856979349dc87b86b
Duders591 <dasjgkdsfhdsjkfadshlk@gmail.com>
2026-03-14 11:02:00 +0100

    Update script.js

--- timer/script.js
+++ timer/script.js
@@ -2,13 +2,7 @@ console.log("Javascript connected successfully!");
 const inputs = document.getElementById("inputs");
 function checkIfValid(input) {
 let inputNumber = input.valueAsNumber;
-    if (inputNumber < 0) {
-        input.value = "";
-        alert("Geen negatieve getallen invullen!");
-	} else if (inputNumber > 60) {
-		input.value = "";
-		alert("Geen getallen boven de 60!");
-    } else if (Number.isNaN(inputNumber)) {
+if (Number.isNaN(inputNumber)) {
         input.value = "";
         alert("Geen letters/leestekens invullen!");
     } else if (inputs.children[0].value !== "" && inputs.children[1].value !== "" && inputs.children[2].value !== "") {
